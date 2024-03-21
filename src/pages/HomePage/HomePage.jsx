17abc7572738e690aef5ca181445a7757163f897
Maria Fortuny <dissenyfortuny@gmail.com>
2024-03-21 18:46:08 +0100

    first-homepage-creation

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import { Flex } from '@chakra-ui/layout'
+import PageWrapper from '../../components/PageWrapper/PageWrapper'
+import React from 'react'
+
+
+function HomePage() {
+    return (
+        <PageWrapper>
+            <Flex>
+
+
+            </Flex>
+        </PageWrapper>
+    )
+}
+
+export default HomePage
