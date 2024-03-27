135d902d0a6481983764f1d1ebefd05c867cf947
Maria Fortuny <dissenyfortuny@gmail.com>
2024-03-27 20:59:20 +0100

    create-first-commit

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -21,7 +21,11 @@ const AppRoutes = () => {
                     path: "/cats",
                     element: <CatsPage />,
                     loader: LoaderCatsPage
-                }
+                },
+                {
+                    path: "/cats/create",
+                    element: <div>ei</div>,
+                },
             ]
         },
         {
